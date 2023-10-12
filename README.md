0cf4e64f5b622c8dc064a99892ddb1449ec846cc
lauradreith <reitlaur@amazon.com>
2023-10-11 17:03:35 -0700

    readme and function update

--- README.md
+++ README.md
@@ -1,11 +1,59 @@
-## My Project
+# Multiple Instance Management Iam Identity Center
 
-TODO: Fill this README out!
 
-Be sure to:
+Multiple account instances of AWS IAM Identity Center can now be deployed in your AWS organization to support authentication for AWS managed applications, such as Amazon Redshift. This solution generates reports to provide visibility into users across multiple IAM Identity Center instances deployed in your AWS organizations. The solution comprises of a script that generates information to help you manage multiple IAM Identity Center instances. The python script will provide you with the following reports:
 
-* Change the title in this README
-* Edit your repository description on GitHub
+
+1.	**Account instances summary:** a report with the accounts and IAM Identity Center instances deployed in each of them.
+2.	**User duplication:** a report of all duplicated users and the IAM iIdentity Center instances they belong to. A duplicated user is one that appears in more than one IAM Identity Center instance. Email compares users.
+3.	**Application assignments on local instances:** a report with the users assigned to each application
+
+## Solution Overview
+![alt diagram](/image/RepoDiagram.png)
+
+
+
+Once you deploy the solution, the AWS Lambda will do the following:
+1.	Gets triggered based on the set cron schedule
+2.	Lists all accounts in the AWS Organizations
+3.	Get the Organization instance of IAM Identity Center Id
+4.	Assume a role in each member account
+5.	List Account instance of IAM Identity Center, and capture the Id
+6.	List applications assigned to each IAM Identity Center instance
+7.	List user assignments to each application
+8.	Generates and uploads the reports to the Amazon S3 bucket named idc-report-<StackId>
+
+## How to use this solution?
+
+Scripts and templates are available in the repository. Use them how is more comfortable for you. The suggested procedure is to deploy this script in an AWS Lambda in the account where your IAM Identity Center is managed or in the account assigned as a [Delegated administrator for AWS Organizations](https://docs.aws.amazon.com/organizations/latest/userguide/orgs_delegate_policies.html). 
+To successfully deploy this solution you will need a AWS Lambda deployment package, an S3 bucket to upload the package, and an IAM role that can be assumed by Lambda in each member account. 
+
+
+Follow the steps to get started:
+
+
+1. **Deploy the CloudFormation template:** 
+- There are two CloudFormation templates in this repository, the member-account-role.yaml and the management-account-infrastructure.yaml. 
+    - If you do not have a cross account role, you can use the member-account-role.yaml to create one. You can use [CloudFormation StackSets](https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/what-is-cfnstacksets.html) to deploy this template across multiple accounts. To deploy this template you will need to input your management or delegated admin account ID.
+    - The management-account-infrastructure.yaml file will create resources needed for this solution. To deploy this template you will need to input the name of the S3 bucket where your deployment package is, and the cross account role name. If you used the member-account-role.yaml to create the role, you can leave the default value on the CrossAccountRoleName parameter. 
+
+3.	**Deployment considerations:**
+-	Cross account role: The AWS Lambda function needs access to your member accounts, for this you need to provide a cross account role. The permissions needed for lambda are listed on the “Require permissions section of this repo”
+-	Create a cross account role for Lambda: If you are using an existing role, make sure that all the permissions needed are included on that role. You can see the permissions in the next section.
+-	Once the management-account-infrastructure.yaml finishes deploying the resources, you can trigger your new Lambda and  review the report on the Amazon S3 bucket named idc-report-<StackId>
+- The Lambda function is scheduled to be triggered once a day at 12:00 AM UTC. 
+
+## Other Considerations
+
+1.	The Lambda execution role will need:
+-	AWS Organizations Read permissions
+-	AWS IAM Identity Center Read Permissions
+-	Permissions to assume a role in each child account in the organization
+-   Permissions to put object on the S3 bucket
+2. AWS Lambda cross account role:
+- sso:ListInstances
+- identitystore:ListUsers
+- Assume role policy: Principal: AWS: arn:aws:iam::${ManagementAccountId}:role/lambda-execution-role
 
 ## Security
 
---
+++ lambda_function/lambda_function.py
@@ -0,0 +1,166 @@
+import argparse
+import json
+import boto3
+import logging
+import datetime
+import csv
+import os
+
+accounts_and_instances_dict={}
+duplicated_users ={}
+
+main_session = boto3.session.Session()
+sso_admin_client = main_session.client('sso-admin')
+identity_store_client = main_session.client('identitystore')
+organizations_client = main_session.client('organizations')
+s3_client = boto3.client('s3')
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+def lambda_handler(event, context):
+    application_assignment = []
+    user_dict={}
+    
+    current_account = os.environ['CurrentAccountId']
+ 
+    logger.info("Current account %s", current_account)
+    
+    paginator = organizations_client.get_paginator('list_accounts')
+    page_iterator = paginator.paginate()
+    for page in page_iterator:
+        for account in page['Accounts']:
+            get_credentials(account['Id'],current_account)
+            #get all instances per account - returns dictionary of instance id and instances ARN per account
+            accounts_and_instances_dict = get_accounts_and_instances(account['Id'], current_account)
+            user_dict = get_users(accounts_and_instances_dict[account['Id']][0],user_dict)
+            application_assignment = get_application_assignment(accounts_and_instances_dict[account['Id']][1],application_assignment)
+
+    construct_summary(application_assignment, accounts_and_instances_dict, duplicated_users)
+
+
+
+                    
+def get_accounts_and_instances(account_id, current_account):
+    global accounts_and_instances_dict
+    
+    instance_paginator = sso_admin_client.get_paginator('list_instances')
+    instance_page_iterator = instance_paginator.paginate()
+    for page in instance_page_iterator:
+        for instance in page['Instances']:
+            #send back all instances and identity centers
+            if account_id == current_account:
+                accounts_and_instances_dict = {current_account:[instance['IdentityStoreId'],instance['InstanceArn']]}
+            elif instance['OwnerAccountId'] != current_account: 
+                accounts_and_instances_dict[account_id]= ([instance['IdentityStoreId'],instance['InstanceArn']])
+    return accounts_and_instances_dict
+    
+#determine if the member IdentityStores have duplicate and update the dictionary
+def get_users(identityStoreId, user_dict): 
+    global duplicated_users
+    paginator = identity_store_client.get_paginator('list_users')
+    page_iterator = paginator.paginate(IdentityStoreId=identityStoreId)
+    for page in page_iterator:
+        for user in page['Users']:
+            if ( 'Emails' not in user ):
+                print("user has no email")
+            else:
+                for email in user['Emails']:
+                    if email['Value'] not in user_dict:
+                        user_dict[email['Value']] = identityStoreId
+                    else:
+                        print("Duplicate user found " + user['UserName'])
+                        user_dict[email['Value']] = user_dict[email['Value']] + "," + identityStoreId
+                        duplicated_users[email['Value']] = user_dict[email['Value']]
+    return user_dict
+    
+
+def get_credentials(account_id,current_account):
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    global sso_admin_client
+    global identity_store_client
+    role_name = os.environ['CrossAccountRoleName']
+    #for mgmt/delegated admin account use the current session
+    # for all other accounts assume role into the account and use the assumed role session
+    if account_id == current_account :
+        sso_admin_client = main_session.client('sso-admin')
+        identity_store_client = main_session.client('identitystore')
+    else:
+        role_arn = f"arn:aws:iam::{account_id}:role/{role_name}"
+        logger.info("Switching to %s", role_arn)
+        sts = main_session.client('sts')
+        assume_role_response = sts.assume_role(
+        RoleArn=role_arn, RoleSessionName="CrossAccountMemberRoleSession")
+        sso_admin_client = boto3.client(
+            'sso-admin',
+            aws_access_key_id=assume_role_response['Credentials']['AccessKeyId'],
+            aws_secret_access_key=assume_role_response['Credentials']['SecretAccessKey'],
+            aws_session_token=assume_role_response['Credentials']['SessionToken'])
+        identity_store_client = boto3.client('identitystore',aws_access_key_id=assume_role_response['Credentials']['AccessKeyId'],
+            aws_secret_access_key=assume_role_response['Credentials']['SecretAccessKey'],
+            aws_session_token=assume_role_response['Credentials']['SessionToken'])
+    
+def get_application_assignment(instanceARN, application_assignment):
+    applications = sso_admin_client.list_applications(InstanceArn = instanceARN)
+    
+    for application in applications['Applications']:
+        all_assignments = sso_admin_client.list_application_assignments(ApplicationArn = application['ApplicationArn'])
+        if not all_assignments['ApplicationAssignments']:
+            print("There are no assignments for the application ")
+        else:
+            application_assignment.append({'Name':application['Name'], 'Application ARN': application['ApplicationArn'], 'Users':[]})
+            for item in all_assignments['ApplicationAssignments']:
+                application_assignment[len(application_assignment)-1]['Users'].append(item['PrincipalId'])
+    return application_assignment
+    
+def csv_to_S3(logsDic, path):
+    #create cvs file that will be uploaded to Amazon s3
+    bucket = os.environ['reports_bucket']
+    now = datetime.datetime.now()
+    LambdaTemp= '/tmp/summary_file.csv'
+    
+    if path == 'idc_instances':
+        prefix = 'sample/' + str(now) + '_idc_instances_'+'summary_file.csv'
+        fields = ['accountId', 'IdentityCenterInstance', 'Identity Store Arn']
+    elif path == 'duplicated_users':
+        prefix = 'sample/' + str(now) + '_duplicated_users_'+'summary_file.csv'
+        fields = ['User_email', 'IdentityStoreId']
+    elif path == 'application_assignment':
+        prefix = 'sample/' + str(now) + '_application_assignment_'+'summary_file.csv'
+        fields = ['Name', 'Application ARN', 'Users']
+    fieldnames = 'summary_file.csv'
+    with open (LambdaTemp, "w+") as f:
+        writer = csv.DictWriter(f, fieldnames = fields)
+        writer.writeheader()
+        writer.writerows(logsDic)
+        #fix self
+    try:
+        s3_client.upload_file('/tmp/summary_file.csv', bucket, prefix)
+    except Exception as ex:
+        self.log_error(f'Error upload file to S3 for idc_instances. Error: {str(ex)}')
+        raise
+
+    return ("Return to next summary")
+
+def construct_summary(application_assignment, accounts_and_instances_dict, duplicated_users):
+    duplicated_users_summary = []
+    identity_center_instances_summary = []
+    application_assignment_summary =[]
+    for item in duplicated_users:
+        duplicated_users_summary.append({'User_email':item, 'IdentityStoreId':duplicated_users[item]})
+    csv_to_S3(duplicated_users_summary, 'duplicated_users')
+    for item in accounts_and_instances_dict:
+        identity_center_instances_summary.append({'accountId':item, 'IdentityCenterInstance':accounts_and_instances_dict[item][0], 'Identity Store Arn':accounts_and_instances_dict[item][1]})
+    csv_to_S3(duplicated_users_summary, 'duplicated_users')
+    for item in application_assignment:
+        application_assignment_summary.append({'Name':item['Name'], 'Application ARN':item['Application ARN'],'Users':item['Users']})
+    csv_to_S3(application_assignment_summary, 'application_assignment')
+    return ("Summary is on s3")
+
+
+if __name__ == "__main__":
+    event = {}
+    context = "this is a context"
+    lambda_handler(event,context)
